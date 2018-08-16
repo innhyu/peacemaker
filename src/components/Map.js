ea1780400cc24c03c998b854cb48935b25635808
innhyu <innhyu@gmail.com>
2018-08-16 00:46:44 -0500

    Map: Changed the way the markers are set; working modular and well

--- src/components/Map.js
+++ src/components/Map.js
@@ -15,38 +15,45 @@ class Map extends Component {
             marker: null
         };
 
-        this.setupMap = this.setupMap.bind(this);
     }
 
     componentDidMount() {
-        this.setupMap();
+        this.setupMapAndMarker();
         this.props.mapLoad(this.props.userId);
     }
 
     /**
-     * Function that sets up the map div and saves the actual object to the state
+     * Function that return (renders really, but no work with rendering here) the map container
+     * after doing some pre-processes
      */
-    setupMap() {
-        const container = document.getElementById(this.props.userId);
-        const options = { //지도를 생성할 때 필요한 기본 옵션
-            center: new window.daum.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-            level: 4 //지도의 레벨(확대, 축소 정도)
-        };
-        const map = new window.daum.maps.Map(container, options);
-
-        this.setState({
-            map: map
-        });
-
-    }
+    renderMapContainer = () => {
+        this.moveMapAndMark();
+        return (<div id={this.props.userId} style={this.props.style}/>);
+    };
 
     /**
-     * Removes marker on map if it exists
+     * Function that sets up the map div and saves the actual object to the state
      */
-    removeMarker = () => {
-        if(this.state.marker){
-            this.state.marker.setMap(null);
-        }
+    setupMapAndMarker = () => {
+        const container = document.getElementById(this.props.userId);
+        const options = {
+            center: new window.daum.maps.LatLng(33.450701, 126.570667),
+            level: 4
+        };
+        // Creating a map
+        const map = new window.daum.maps.Map(container, options);
+
+        // Creating a marker and hooking it up to the map
+        const marker = new window.daum.maps.Marker({
+            position: new window.daum.maps.LatLng(33.450701, 126.570667)
+        });
+        marker.setMap(map);
+
+        this.setState({
+            map: map,
+            marker: marker
+        });
+
     };
 
     /**
@@ -58,27 +65,14 @@ class Map extends Component {
             const x = selfMap.x;
             const y = selfMap.y;
             const coords = new window.daum.maps.LatLng(y, x);
-            const marker = new window.daum.maps.Marker({
-                position: coords
-            });
 
             this.state.map.setCenter(coords);
-            marker.setMap(this.state.map);
-
-            this.setState({
-                marker: marker
-            });
+            this.state.marker.setPosition(coords);
         }
     };
 
     render() {
-
-        this.removeMarker();
-        this.moveMapAndMark();
-
-        return (
-            <div id={this.props.userId} style={this.props.style}/>
-        );
+        return this.renderMapContainer();
     }
 }
 
