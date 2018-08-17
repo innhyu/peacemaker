1e65513b710e77a3aa2c6e3a73990ac745b6e2ea
innhyu <innhyu@gmail.com>
2018-08-17 03:57:19 -0500

    Added a way to compute midpoint

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React, {Component} from 'react';
-import {Container, Row, Col} from 'reactstrap';
+import {Container, Row, Col, Button} from 'reactstrap';
 
 import './App.css';
 import UserAddress from "./components/UserAddress";
 
 export default class App extends Component {
-  render() {
-    return (
-        <Container>
-          <Row>
-            <Col xs='12' md={{size: 6}}>
-              <UserAddress />
-            </Col>
-            <Col xs='12' md={{size: 6}}>
-              <UserAddress />
-            </Col>
-          </Row>
-        </Container>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col xs='12' md={{size: 6}}>
+                        <UserAddress/>
+                    </Col>
+                    <Col xs='12' md={{size: 6}}>
+                        <UserAddress/>
+                    </Col>
+                    <Col xs='12' md={{size: 12}}>
+                        <Button size='lg' color="secondary" onClick={() => {}}>Find Something</Button>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 const styles = {};
---
+++ src/util/Wayfinder.js
@@ -0,0 +1,65 @@
+/**
+ * Utility that helps find the midpoint between multiple positions
+ */
+export default class WayFinder {
+
+    /**
+     * Function that finds the midpoint given all positions in lon / lat
+     *
+     * @param positions - array of objects with lat and lon
+     */
+    static midPoint(positions){
+        const x = [];
+        const y = [];
+        const z = [];
+
+        positions.forEach(position => {
+            x.push(WayFinder.xOf(position));
+            y.push(WayFinder.yOf(position));
+            z.push(WayFinder.zOf(position));
+        });
+
+        const midX = x.reduce((a,b) => a + b) / positions.length;
+        const midY = y.reduce((a,b) => a + b) / positions.length;
+        const midZ = z.reduce((a,b) => a + b) / positions.length;
+
+        const newLonRad = Math.atan2(midY, midX);
+        const hyp = Math.sqrt(midX * midX + midY * midY);
+        const newLatRad = Math.atan2(midZ, hyp);
+
+        const newLon = newLonRad * (180 / Math.PI);
+        const newLat = newLatRad * (180 / Math.PI);
+
+        return {newLon, newLat}
+    }
+
+    /**
+     * Function that computes the x coordinate given lon / lat
+     */
+    static xOf({lat, lon}){
+        return Math.cos(WayFinder.toRad(lat)) * Math.cos(WayFinder.toRad(lon));
+    }
+
+    /**
+     * Function that computes the y coordinate given lon / lat
+     */
+    static yOf({lat, lon}){
+        return Math.cos(WayFinder.toRad(lat)) * Math.sin(WayFinder.toRad(lon));
+    }
+
+    /**
+     * Function that computes the z coordinate given lon / lat
+     */
+    static zOf({lat}){
+        return Math.sin(WayFinder.toRad(lat));
+    }
+
+    /**
+     * Function that returns the input in radians
+     */
+    static toRad(notRad){
+        return notRad * (Math.PI / 180);
+    }
+
+}
+
